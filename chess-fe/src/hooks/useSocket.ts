f9629216b9abd599d598517398f5d511514f92fc
Nagu <nagukalakuntla@gmail.com>
2024-08-31 16:55:49 +0530

    Fixed the useSocket link

--- chess-fe/src/hooks/useSocket.ts
+++ chess-fe/src/hooks/useSocket.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-const WS_URL = "https://chess-n-chat.vercel.app/";
+const WS_URL = "ws://localhost:8080";
 
 export const useSocket = () => {
     const [socket, setSocket] = useState<WebSocket | null>(null);
